2799a41dfa5a4d962d610c40f613c3dfc6854442
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-11-06 23:21:53 -0500

    Changed the title text for the about us link

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
-<header>
+<header id="header">
     <a href="index.php" class="logo" title="Return to the homepage">Lily's Lil Garage</a>
     <nav>
         <a href="locations.php" title="View our store locations">Locations</a>
         <a href="menu.php" title="Browse our menu">Menu</a>
         <a href="contact.php" title="View our contact information">Contact</a>
-		<a href="aboutUs.php" title="Select your preffered location">About Us</a>
-		<a href="orderOnline.php" title="Click here to make an online order">Order Online</a>
+	<a href="aboutUs.php" title="View more information about our company">About Us</a>
+	<a href="orderOnline.php" title="Click here to make an online order">Order Online</a>
     </nav>
 </header>
