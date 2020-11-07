f4172ec110b9959b48e3a57c4bf3e30ad6a6a1dc
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-11-06 23:11:21 -0500

    slight tweaks to layout

--- Scripts/mainScripts.js
+++ Scripts/mainScripts.js
@@ -7,9 +7,9 @@ function HeaderScroll() {
   // Changes the styling on the webpage header when the user scrolls the mouse down from the top of the page
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     // When the webpage is scrolled 50px or more
-    document.getElementById("header").style.cssText = "height: 6.25vh; max-height: 7vh; padding: 1vh 3%; box-shadow: 0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28); ";
+    document.getElementById("header").style.cssText = "height: 68px; max-height: 68px; padding: 1vh 3%; box-shadow: 0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28);";
   } else {
     // When the webpage is not scrolled
-    document.getElementById("header").style.cssText = "height: 9.5vh; max-height: 9.5vh;";
+    document.getElementById("header").style.cssText = "height: 96px; max-height: 96px;";
   }
 } 
