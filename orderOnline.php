c18a060465e50bcbd5fa02f9da9b4f2b8229a52a
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-11-29 01:01:16 -0500

    basic order page mockup/database test
    -pulls data from sql db
    -basic layout built

--- orderOnline.php
+++ orderOnline.php
@@ -29,23 +29,80 @@ require ('Includes/databaseHandler.php')
         <script src="Scripts/mainScripts.js"></script> 
     </head>
     <body>
+        <?php
+            $location = $_POST['location'];
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $email_address = $_POST['email_address'];
+            $phone_number = $_POST['phone_number'];
+        ?>
         <header id="orderHeader">
             <a href="index.php" class="logo" title="Return to the homepage">Lily's Lil Garage</a>
-            <div id="orderStoreInfo">
-                <h2>LLG Flushing</h2>
-                <p>165 Boman Street</p>
-                <p>Flushing, Michigan 48433</p>
-                <p>(345) 678-9012</p>
-            </div>
+            <?php include('storeHeader.php'); ?>
         </header>
 
         <!-- Main Webpage Content-->
         <main id="orderMain">
+            <div class="orderLeft">
+                <h4>Food Types</h4>
+                <p>combos</p>
+                <p>pancakes</p>
+                <p>crepes</p>
+                <p>omelets</p>
+                <p>entree plates</p>
+                <p>burgers &amp; handhelds</p>
+                <p>pastas</p>
+                <p>wraps</p>
+                <p>finger foods</p>
+                <p>salads</p>
+                <p>non-alcoholic beverages</p>
+                <p>alcoholic beverages</p>
+                <p>desserts</p>
+            </div>
+            <div class="orderCenter">
+                <table>
+                <?php
+                    // Select menu itms
+                    $sql = 'SELECT * FROM menu_items';
+                    $result = $conn-> query($sql);
 
+                    if ($result-> num_rows > 0) {
+                        while ($row = $result-> fetch_assoc()) {
+                            echo "<tr><td>". $row["food_name"] ."</td><td>". $row["price"] ."</td><td>". $row["description"] ."</td></tr>";
+                        }
+                        echo "</table>";
+                    }
+                    else {
+                        echo "0 results";
+                    }
+                ?>
+            </div>
+            <div class="orderRight">
+                <div class="orderUserInfo">
+                    <?php
+                        echo "<h4>Ordering For:</h4>";
+                        echo "<p>$first_name</p>";
+                        echo "<p>$last_name</p>";
+                        echo "<p>$email_address</p>";
+                        echo "<p>$phone_number</p>";
+                    ?>
+                </div>
 
-            <?php
-
-            ?>
+                <div class="orderCart">
+                    <table>
+                        <tbody>
+                            <tr><td>placeholder item</td><td>(price)</td></tr>
+                            <tr><td>placeholder item</td><td>(price)</td></tr>
+                            <tr><td>placeholder item</td><td>(price)</td></tr>
+                            <tr><td>placeholder item</td><td>(price)</td></tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="orderCheckout">
+                    <h4>Total Price: placeholder</h4>
+                    <button value="checkout">Checkout</button>
+                </div>
+            </div>
         </main>
 
         <!-- Webpage Footer-->
