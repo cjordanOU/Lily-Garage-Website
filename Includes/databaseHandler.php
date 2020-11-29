37cc0ec5a304f4dc0012d4eaae56a1fb4f100a69
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-11-29 00:58:18 -0500

    database handler rewrite

--- Includes/databaseHandler.php
+++ Includes/databaseHandler.php
@@ -2,31 +2,32 @@
 
 // Specifies location of database and database name
 // This name needs to be changed when hosted online
-$dbServerName = 'mysql:host=localhost:3306;dbname=llg';
+$dbLocation = 'localhost:3306';
+$dbName = 'llg';
 
 // Database username and password
 $dbUsername = 'root';
 $dbPassword = '';
 
 
-// Database login
-try {
-    $db = new PDO($dbServerName, $dbUsername, $dbPassword);
-}
-catch (PDOexception $e) {
-    $error_message = $e->getMessage();
-    header('Location: index.php?action=error&message=$error_message');
+// Database connection
+$conn = mysqli_connect("$dbLocation", "$dbUsername", "$dbPassword", "$dbName");
+if ($conn-> connect_error) {
+    die("Connection failed: ". $conn-> connect_error);
 }
 
+
+
 // Count menu items
+/*
 function countMenuItems() {
     global $db;
-    $query = 'SELECT COUNT(*) FROM users';
+    $query = 'SELECT COUNT(*) FROM menu_items';
     $statement = $db->prepare($query);
     $statement->execute();
     $result = $statement->fetch();
     $count = $result[0];
     return count;
-}
+}*/
 
 ?>
