96ce48036dde560eed0483c6d226540bcb570230
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-11-30 01:47:18 -0500

    fixed main links leading to .html instead of .php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
             <div class="parallaxBG-large" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.801), transparent), url('Images/RestaurantOutsideBlur.jpg');"></div>
             <div class="parallaxText1">
                 <h2>Welcome to Lily's Lil Garage</h2>
-                <a href="orderOnline.html">Order Online Now</a>
+                <a href="orderUser.php">Order Online Now</a>
             </div>
             
             <div class="standardPad">
@@ -55,7 +55,7 @@
 						<div id="homeSlideshowContainer">
 						<img id="slideshowImage" src=" " alt="A slideshow of our delicious foods and drinks" width="420" height="320">
 						<center><p>Feel free to browse our menu by clicking the button below.</p></center>
-						<a href="menu.html" title="Browse our menu">View Our Menu</a>
+						<a href="menu.php" title="Browse our menu">View Our Menu</a>
 						</div>
 			
             <div id="homeLocation">
@@ -63,7 +63,7 @@
                 <section>
                     <h2 class="noMargin">We've got what you want, when you need it</h2>
                     <p>We work our hardest to bring exactly what our customers want. With menu items tailored specifically to each restaurant, you're sure to find something you'll love.</p>
-                    <a class="linkButton" href="locations.html" title="Choose a location">Choose a location</a>
+                    <a class="linkButton" href="locations.php" title="Choose a location">Choose a location</a>
                 </section>
                 <img src="Images/RadiatorBurger.JPG">
             </div>
