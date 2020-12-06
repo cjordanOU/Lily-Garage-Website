9c20af549122bde4b49f9674e7e2a1da6890b04b
Cam <71766182+cjordanOU@users.noreply.github.com>
2020-12-06 15:34:32 -0500

    Completed main order logic functions

--- Scripts/orderLogic.js
+++ Scripts/orderLogic.js
@@ -1,4 +1,7 @@
+// Global Variables
+var totalPrice = 0.0;
 
+// Add to Cart function used on 'orderOnline.php'
 function addToCart(itemName, itemPrice) {
     // Create the element
     var orderItem = document.createElement("p");
@@ -12,10 +15,24 @@ function addToCart(itemName, itemPrice) {
     cartLocation.appendChild(orderItem);
     cartLocation.appendChild(orderPrice);
 
-    // Calculates the final price
-    var totalPrice = 0;
-    totalPrice = itemPrice + totalPrice;
-    var totalLocation = document.getElementById("orderTotalPrice");
-    totalLocation.innerHTML = "$" + totalPrice;
+    // Update the total price
+    updateTotalPrice(itemPrice);
+
+    // Enable checkout button
+    document.getElementById("orderCheckoutButton").disabled = false;
 }
 
+// Updates the total price of the ordered food
+function updateTotalPrice(value) {
+    var singlePrice = value;
+    singlePrice = parseFloat(singlePrice);
+    totalPrice = totalPrice + singlePrice;
+
+    // Fixes rounding errors from the float
+    // Changes output to string, dont use totalPricePoint in math
+    totalPricePrint = totalPrice.toFixed(2);
+
+    // Changes the total price on the page
+    var totalLocation = document.getElementById("orderTotalPrice");
+    totalLocation.innerHTML = "Total Price:  $" + totalPricePrint;
+}
